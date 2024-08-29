b45b13800ba5cc318fcdfb69f9616f245b271557
deteto_chudo <127776832+deteto747chudo@users.noreply.github.com>
2024-08-29 21:39:17 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run the Python scripts sequentially
+python3 mainsetup.py
+python3 mainsetup2.py
+python3 mainsetup3.py
+
+# Optional: echo a message indicating the completion of the scripts
+echo "All Python scripts have been executed."
